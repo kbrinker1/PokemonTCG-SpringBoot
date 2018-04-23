62e0c797b8037f9035f9cc86b0240a44700e5ac1
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-04-23 14:35:39 -0400

    setting up

--- src/main/java/pokemontcgproject/pokemontcgapp/PokemonTCGApplication.java
+++ src/main/java/pokemontcgproject/pokemontcgapp/PokemonTCGApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PokemontcgappApplication {
+public class PokemonTCGApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PokemontcgappApplication.class, args);
+		SpringApplication.run(PokemonTCGApplication.class, args);
 	}
 }
